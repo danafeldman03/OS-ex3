603c9fad3eb0164cb46fc175b86ac4186b7f2895
danafeldman03 <dana.feldman.43@gmail.com>
2026-06-17 23:54:01 +0300

    placeholder for page fault handler

--- src/VirtualMemory.cpp
+++ src/VirtualMemory.cpp
@@ -10,8 +10,8 @@
 * @return 1 on success, 0 on failure (if the address cannot be
 * mapped to a physical address for any reason).
 */
-int translateVirtualAddress(uint64_t virtualAddress, word_t* physicalAddress) {
-    word_t addr = 0;
+int translateVirtualAddress(uint64_t virtualAddress, uint64_t* physicalAddress) {
+    uint64_t addr = 0;
     uint64_t rootIndex = virtualAddress >> (TABLES_DEPTH * OFFSET_WIDTH);
     if(!PMread(rootIndex, &addr))
     {
@@ -20,6 +20,10 @@ int translateVirtualAddress(uint64_t virtualAddress, word_t* physicalAddress) {
 
     for(int level = 1; level < TABLES_DEPTH; ++level)
     {
+        if(addr == 0)
+        {
+            return pageFaultHandler(virtualAddress);
+        }
         int shift = (TABLES_DEPTH - level) * OFFSET_WIDTH;
         uint64_t index =(virtualAddress >> shift)&((1ULL << OFFSET_WIDTH) - 1);
         if(!PMread(addr * PAGE_SIZE + index, &addr))
@@ -27,11 +31,23 @@ int translateVirtualAddress(uint64_t virtualAddress, word_t* physicalAddress) {
             return 0;
         }
     }
+    if(addr == 0)
+    {
+        return pageFaultHandler(virtualAddress);
+    }
     uint64_t offset = virtualAddress & ((1ULL << OFFSET_WIDTH) - 1);
     *physicalAddress = addr * PAGE_SIZE + offset;
+    if(virtualAddress >= VIRTUAL_MEMORY_SIZE)
+    {
+        return 0;
+    }
     return 1;
 }
 
+int pageFaultHandler(uint64_t virtualAddress) {
+    //placeholder for page fault handling logic
+    return 0; // Return 0 to indicate failure for now
+}
 
 
 
@@ -45,19 +61,18 @@ void VMinitialize() {
 }
 
 int VMread(uint64_t virtualAddress, word_t* value) {
-    word_t physicalAddress;
+    uint64_t physicalAddress;
     if(translateVirtualAddress(virtualAddress, &physicalAddress))
     {
         PMread(physicalAddress, value);
         return 1;
     }
     return 0;
-
 }
 
 
 int VMwrite(uint64_t virtualAddress, word_t value) {
-    word_t physicalAddress;
+    uint64_t physicalAddress;
     if(translateVirtualAddress(virtualAddress, &physicalAddress))
     {
         PMwrite(physicalAddress, value);
@@ -66,4 +81,5 @@ int VMwrite(uint64_t virtualAddress, word_t value) {
     return 0;
 }
 
+//TODO
 uint64_t VMgetMapping(uint64_t virtualPage) {} 
