6d29377a0b17d075e13e13aa7ee7f6100bf2f44c
danafeldman03 <dana.feldman.43@gmail.com>
2026-06-24 21:12:48 +0300

    after first fix

--- src/VirtualMemory.cpp
+++ src/VirtualMemory.cpp
@@ -1,6 +1,41 @@
 #include "VirtualMemory.h"
 #include "MemoryConstants.h"
+#include "PhysicalMemory.h"
 #include <algorithm>
+
+//------------------------------Declerations------------------------------------
+struct frameSearchInfo{
+    //case 1: frame is empty
+    bool foundEmptyTable = false;
+    word_t emptyFrame = 0;
+    word_t emptyFrameParent = 0;
+    uint64_t emptyFrameIndex = 0;
+
+    //case 2: unused frame
+    word_t maxFrameIndex = 0;
+
+    //case 3: evictFrame
+    word_t evictFrame = 0;
+    word_t evictFrameParent = 0;
+    uint64_t evictParentIndex = 0;
+    uint64_t evictPage = 0;
+    uint64_t distance = 0;
+    bool foundEvictVictim = false;
+};
+
+uint64_t getIndex(uint64_t virtualAddress, int level);
+bool isEmptyTable(word_t frame);
+uint64_t calculateDistance(uint64_t a, uint64_t b);
+frameSearchInfo findFrame(uint64_t virtualPage, word_t noEvictFrame);
+void dfs(word_t frame, word_t parentFrame, uint64_t parentIndex, int level,
+     uint64_t currentPage, uint64_t targetPage, word_t noEvictFrame, frameSearchInfo& info);
+word_t pageFaultHandler(uint64_t virtualPage, word_t parentFrame, uint64_t parentIndex, 
+    int level, word_t noEvictFrame);
+int translateVirtualAddress(uint64_t virtualAddress, uint64_t* physicalAddress);
+
+
+
+
 
 //------------------------------Helper Functions------------------------------------
 
